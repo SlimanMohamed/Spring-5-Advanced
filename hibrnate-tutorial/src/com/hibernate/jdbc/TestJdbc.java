4bfeaa771d69edcd868018ebb65180310a6e7f22
SLIMAN Mohamed <mohamedsliman789@gmail.com>
2022-04-18 23:52:14 +0000

    Hibernate Configuration with Annotations

---
+++ hibrnate-tutorial/src/com/hibernate/jdbc/TestJdbc.java
@@ -0,0 +1,25 @@
+package com.hibernate.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		
+		String jdbc="jdbc:mysql://localhost/hibernate";
+		String user="root";
+		String password="";
+		
+		try {
+			
+			Connection cnx = DriverManager.getConnection(jdbc,user,password);
+			System.out.println("connection worked !!!!");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
